1184b25267236a1cd653c479012bcfe100c1f5bd
Eduardo Pilati <pilatieduardo@gmail.com>
2023-12-25 22:00:30 -0300

    Development

---
+++ src/main/java/dev/pilati/damagetest/DamageTest.java
@@ -0,0 +1,87 @@
+package dev.pilati.damagetest;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class DamageTest extends JavaPlugin implements Listener {
+    protected final String METADATA_KEY = "damageTest";
+
+    protected void addPlayerMetadata(Player player) {
+        player.setMetadata(METADATA_KEY, new FixedMetadataValue(this, true));
+    }
+
+    protected void removePlayerMetadata(Player player) {
+        player.removeMetadata(METADATA_KEY, this);
+    }
+
+    protected boolean hasPlayerMetadata(Player player) {
+        return player.hasMetadata(METADATA_KEY);
+    }
+
+    protected String getMessage(String key){
+        String message = getConfig().getString(key);
+
+        if (message == null) {
+            return "";
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public void onEnable() {
+        saveDefaultConfig();
+        Bukkit.getPluginCommand("damagetest").setExecutor(new DamageTestCommand());
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if(!(event.getDamager() instanceof Player)) {
+            return;
+        }
+        
+        Player player = (Player) event.getDamager();
+        if(!hasPlayerMetadata(player)) {
+            return;
+        }
+
+        String message = getMessage("message")
+                            .replace("{player}", player.getName())
+                            .replace("{damage}", String.valueOf(event.getFinalDamage()))
+                            .replace("{cause}", event.getCause().name());
+
+        player.sendMessage(message);
+    }
+
+    public class DamageTestCommand implements CommandExecutor {
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            if(!(sender instanceof Player)) {
+                sender.sendMessage(getMessage("onlyPlayers"));
+                return true;
+            }
+
+            Player player = (Player) sender;
+            addPlayerMetadata(player);
+
+            String message = getMessage("command").replace("{status}", 
+                hasPlayerMetadata(player) ? getMessage("enabled") : getMessage("disabled")
+            );
+
+            player.sendMessage(message);
+
+            return true;
+        }
+    }
+}
